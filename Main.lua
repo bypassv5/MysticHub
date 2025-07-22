aa35124ddf914f3f4b6725416fdcc723e2bf1203
bypassv5 <163147680+bypassv5@users.noreply.github.com>
2025-07-22 16:38:46 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,4 @@
+local MysticGui = loadstring(game:HttpGet("https://raw.githubusercontent.com/bypassv5/MysticHub/refs/heads/main/MysticGui.lua")()
+MysticGui:MakeButton("Voidware", "Bedwars, Universal, Keyless", "loadstring(game:HttpGet('https://raw.githubusercontent.com/VapeVoidware/VoidwareBakup/refs/heads/main/NewMainScript.lua'))()")
+MysticGui:MakeButton("Grow A Garden Script", "B")
+MysticGui:MakeButton("Other script", "B")
